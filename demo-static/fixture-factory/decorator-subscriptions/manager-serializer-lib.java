6b7f34e6d73dc56f6e5dbd0a92bff7bec1054231
toemeniaki <toemeniaki@users.noreply.github.com>
2025-11-05 14:48:41 +0000

    Add pages.rs, manager-serializer-lib.java

---
+++ demo-static/fixture-factory/decorator-subscriptions/manager-serializer-lib.java
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/hoavote_z/demo-static/fixture-factory/decorator-subscriptions/manager-serializer-lib.java
+// Generated at: 2025-12-03T23:45:46.415Z
+
+
